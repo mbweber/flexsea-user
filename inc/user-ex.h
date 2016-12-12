9e8f7fc9f69eff1963b8bf0d20effb583771b06f
Luke Mooney <lmooney@dephy.com>
2016-12-12 14:15:29 -0500

    Added current zeros and resistance measurement control.

--- inc/user-ex.h
+++ inc/user-ex.h
@@ -325,14 +325,12 @@ void user_fsm(void);
 		#endif
 	#endif
 
-	#define CURRENT_ZERO		((int32)2065)
-
 	//Encoders:
 	#define ENC_CONTROL			ENC_AS5047
 	#define ENC_COMMUT			ENC_AS5047
 	#define ENC_DISPLAY			ENC_CONTROL
 
-	//Subproject A: Left execute board looking at the back of the ankle while it is standing up
+	//Subproject A: No torque sensor, execute 2
 	#if(ACTIVE_SUBPROJECT == SUBPROJECT_A)
 
 		//Control encoder function:
@@ -341,13 +339,15 @@ void user_fsm(void);
 		#define CTRL_ENC_FCT(x) 	(x)
 		#define CTRL_ENC_VEL_FCT(x) (x)
 		//...
-
+        
+        #define CURRENT_ZERO		((int32)1981)
+        
 		//Slave ID:
 		#define SLAVE_ID		FLEXSEA_EXECUTE_2
 
 	#endif  //SUBPROJECT_A
 
-	//Subproject B: Right actuator
+	//Subproject B: Has the torque sensor, Execute 1
 	#if(ACTIVE_SUBPROJECT == SUBPROJECT_B)
 
 		//Control encoder function:
@@ -355,6 +355,8 @@ void user_fsm(void);
 		#define CTRL_ENC_FCT(x) (x)
 		#define CTRL_ENC_VEL_FCT(x) (x)
 		//...
+        
+        #define CURRENT_ZERO		((int32)2123)
 
 		//Slave ID:
 		#define SLAVE_ID		FLEXSEA_EXECUTE_1
