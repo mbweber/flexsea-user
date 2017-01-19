23717c9bd9910fc4eae3e9bd1adf76c94edf4953
Jean-Francois Duval <jfduval@aqra.ca>
2017-01-19 13:54:24 -0500

    Re-enabled IMU

--- inc/user-mn.h
+++ inc/user-mn.h
@@ -99,7 +99,7 @@ void user_fsm_2(void);
 	#define USE_COMM			//Requires USE_RS485 and/or USE_USB
 	#define USE_I2C_1			//3V3, IMU & Digital pot
 	//#define USE_I2C_2			//3V3, Expansion
-	//#define USE_IMU				//Requires USE_I2C_1
+	#define USE_IMU				//Requires USE_I2C_1
 	#define USE_FLASH_MEM		//FLASH memory
 	//#define USE_COMM_TEST		//Comm. characterization tool
 
