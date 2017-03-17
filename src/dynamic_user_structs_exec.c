8420ef8133b77683ae8df8ec7bf2123e52d48076
David Weisdorf <weisdorf.david@gmail.com>
2017-03-17 15:36:24 -0400

    usage of dataformat header + test code

--- src/dynamic_user_structs_exec.c
+++ src/dynamic_user_structs_exec.c
@@ -1,33 +1,49 @@
 #include "../inc/dynamic_user_structs.h"
 #include <string.h>
+#include "flexsea_dataformats.h"
 
 /* This works for Plan - Execute only
     Requirements:
 
     - send labels to GUI
     - send data to GUI
-    - keep msg under max size
+    - keep msg under max size 
 
+	- TODO: allow GUI to tell execute which fields it wants set
+	- (one time message in tx_..._w, which would tell us which offsets to send)
 */
 
 struct DynamicUserData_s
 {
     int a;
     int b;
+	int c;
 };
 
 DynamicUserData_t dynamicUserData;
 
 #define DYNAMIC_USER_NUM_FIELDS  2
-const uint8_t fieldTypes[DYNAMIC_USER_NUM_FIELDS] = {1, 1};
-static uint8_t fieldSizes[DYNAMIC_USER_NUM_FIELDS] = {0};
-
+const uint8_t fieldTypes[DYNAMIC_USER_NUM_FIELDS] = {FORMAT_32S, FORMAT_32S};
+const char* fieldLabels[DYNAMIC_USER_NUM_FIELDS] = {"a", "b"}; 
 // Keep your label names short, each character is a byte, which takes a lot of memory to send in one packet
 // If label names are too long we may fail to send meta data info to plan.
-const char* fieldLabels[DYNAMIC_USER_NUM_FIELDS] = {"a", "b"}; 
 
-static uint8_t numOffsetsToSend;
-static uint8_t offsetsToSend[DYNAMIC_USER_NUM_FIELDS];
+/*
+
+#define DYNAMIC_USER_NUM_FIELDS  3
+const uint8_t fieldTypes[DYNAMIC_USER_NUM_FIELDS] = {FORMAT_8U, FORMAT_32S, FORMAT_32S};
+// Keep your label names short, each character is a byte, which takes a lot of memory to send in one packet
+// If label names are too long we may fail to send meta data info to plan.
+const char* fieldLabels[DYNAMIC_USER_NUM_FIELDS] = {"a", "b", "c"}; 
+
+*/
+//static uint8_t fieldSizes[DYNAMIC_USER_NUM_FIELDS] = {0};
+//static uint8_t numOffsetsToSend;
+//static uint8_t offsetsToSend[DYNAMIC_USER_NUM_FIELDS];
+
+/*
+	Don't touch the code below
+*/
 
 void tx_cmd_user_dyn_sendMetaData(uint8_t *shBuf, uint8_t *cmd, uint8_t *cmdType, uint16_t *len)
 {
@@ -65,6 +81,21 @@ void tx_cmd_user_dyn_sendMetaData(uint8_t *shBuf, uint8_t *cmd, uint8_t *cmdType
     *len = index;
 }
 
+float quickSin(int y)
+{
+	y %= 360;
+	float x = y > 180 ? -360+y : y;
+	
+	x = x * 314 / 18000;
+	
+	float x3 = x*x*x;
+	float x5 = x3*x*x;
+	float x7 = x5*x*x;
+	float x9 = x7*x*x;
+	
+	return x - x3/6 + x5/120 - x7/5040 + x9/362880;
+}
+
 void tx_cmd_user_dyn_sendData(uint8_t *shBuf, uint8_t *cmd, uint8_t *cmdType, uint16_t *len)
 {
     *cmd = CMD_USER_DYNAMIC;
@@ -72,13 +103,9 @@ void tx_cmd_user_dyn_sendData(uint8_t *shBuf, uint8_t *cmd, uint8_t *cmdType, ui
     uint16_t index = 0;
 	
 	static int x = 0;
-	if(!x)
-	{
-	dynamicUserData.a++;
-	dynamicUserData.b--;
-	}
-	//x++;
-	//x %= 10;
+	x++;
+	dynamicUserData.a = 100 * quickSin(x);
+	dynamicUserData.b = 100 * quickSin(x/2);
 	
     shBuf[index++] = SEND_DATA;
 
